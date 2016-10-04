c974498461d249071382b06998365f4f830ace28
partizand <tumulusan@gmail.com>
2016-10-04 17:45:44 +0300

    Колдовство с тестами Не работает TestCase одновременно из двух модулей

--- src/bankparser/tests/test_parser_admoney.py
+++ src/bankparser/tests/test_parser_admoney.py
@@ -4,12 +4,12 @@ import bankparser.parser as Parser
 
 class ADMoneyTest(unittest.TestCase):
 
-    bank = 'admoney'
+    bank2 = 'admoney'
     #statfile = 'samples/statement.csv'
 
 
 
-    sampletxt=\
+    sampletxt2=\
                 [
                 '12345;02;26/09/2016;02/10/2016',
                 '',
@@ -17,31 +17,31 @@ class ADMoneyTest(unittest.TestCase):
                 '28/09/2016 17:05:29;12345-000;КЦБ ММВБ;721,84;зачисление;Внешнее зачисление;RUR',
                 '30/09/2016 19:12:00;12345-000;КЦБ ММВБ;0,44;списание;Комиссия;RUR'
                 ]
-
-    def setUp(self):
-        self.parser = Parser.StatementParser(self.bank, self.sampletxt)
+    parser_admoney = Parser.StatementParser(bank2, sampletxt2)
+    # def setUp(self):
+    #     self.parser = Parser.StatementParser(self.bank, self.sampletxt)
 
     def test_bankname(self):
-        self.assertEqual(self.parser.bank,self.bank,'Bank name in parser')
+        self.assertEqual(self.parser_admoney.bank,self.bank,'Bank name in parser')
 
     def test_bankname2(self):
-        self.assertEqual(self.parser.statement.bank,self.bank,'Bank name in statement')
+        self.assertEqual(self.parser_admoney.statement.bank,self.bank,'Bank name in statement')
 
     def test_account(self):
-        self.assertEqual(self.parser.statement.account, '12345-000','account in statement')
+        self.assertEqual(self.parser_admoney.statement.account, '12345-000','account in statement')
 
     def test_line1amount(self):
-        amount = self.parser.statement.lines[0].amount
+        amount = self.parser_admoney.statement.lines[0].amount
         self.assertEqual(amount, 721.84, 'Amount in first line (+)')
 
     def test_line2amount(self):
-        amount = self.parser.statement.lines[1].amount
+        amount = self.parser_admoney.statement.lines[1].amount
         self.assertEqual(amount, -0.44, 'Amount in second line (-)')
 
     def test_linedescr(self):
-        descr = self.parser.statement.lines[0].description
+        descr = self.parser_admoney.statement.lines[0].description
         self.assertEqual(descr, "Внешнее зачисление", 'Description in first line')
 
 
-if __name__ == '__main__':
-    unittest.main()
+# if __name__ == '__main__':
+#     unittest.main()
--- src/bankparser/tests/test_parser_vtb24.py
+++ src/bankparser/tests/test_parser_vtb24.py
@@ -4,16 +4,17 @@ import bankparser.parser as Parser
 
 class Vtb24Test(unittest.TestCase):
 
-    bank = 'vtb24'
+    bank1 = 'vtb24'
     #statfile = 'samples/statement.csv'
 
-    sampletxt=\
+    sampletxt1=\
 ['Номер карты/счета/договора;Дата операции;Дата обработки;Сумма операции;Валюта операции;Сумма пересчитанная в валюту счета;Валюта счета;Основание;Статус',
 '\'40817;2016-09-15 09:02:50;2016-09-15;11388,91;RUR;11388,91;RUR;Зарплата;Исполнено',
 '\'40817;2016-09-18 16:47:57;2016-09-18;-44,30;RUR;-44,30;RUR;Операция по карте XXXXXX. OOO Romashka РФ Оплата товаров и услуг;Исполнено',
 '\'40817;2016-09-18 16:20:25;2016-09-18;-644,00;RUR;-644,00;RUR;Операция по карте XXXXXX. ООО Roga РФ Оплата товаров и услуг;Исполнено',
 '\'40817;2016-09-18 14:19:26;2016-09-18;-1701,00;RUR;-1701,00;RUR;Операция по карте XXXXXX. Pukalka РФ Оплата товаров и услуг;Исполнено']
 
+    parser_vtb24 = Parser.StatementParser(bank1, sampletxt1)
     # def __init__(self, methodName='runTest'):
     #
     #     print('init')
@@ -21,35 +22,30 @@ class Vtb24Test(unittest.TestCase):
     #     super().__init__(methodName)
 
 
-    def setUp(self):
-
-        #dir = os.path.dirname(__file__)
-
-        self.parser = Parser.StatementParser(self.bank, self.sampletxt)
-        #self.parser.parse()
-
+    # def setUp(self):
+    #     self.parser = Parser.StatementParser(self.bank, self.sampletxt)
 
     # def test_parse_float(self):
     #     self.assertEqual(self.parser._parse_float('12,3'),12.3,'Проверка чтения float с зяпятой')
     #     self.assertEqual(self.parser._parse_float('12.3'),12.3,'Проверка чтения float с точкой')
 
     def test_bankname(self):
-        self.assertEqual(self.parser.bank,self.bank,'Имя банка в объекте parser')
+        self.assertEqual(self.parser_vtb24.bank,self.bank,'Имя банка в объекте parser')
 
     def test_bankname2(self):
-        self.assertEqual(self.parser.statement.bank,self.bank,'Имя банка в выписке')
+        self.assertEqual(self.parser_vtb24.statement.bank,self.bank,'Имя банка в выписке')
 
     def test_account(self):
-        self.assertEqual(self.parser.statement.account, '\'40817','Счет в выписке')
+        self.assertEqual(self.parser_vtb24.statement.account, '\'40817','Счет в выписке')
 
     def test_lineamount(self):
-        amount = self.parser.statement.lines[0].amount
+        amount = self.parser_vtb24.statement.lines[0].amount
         self.assertEqual(amount, 11388.91)
 
     def test_linedescr(self):
-        descr = self.parser.statement.lines[0].description
+        descr = self.parser_vtb24.statement.lines[0].description
         self.assertEqual(descr, "Зарплата")
 
 
-if __name__ == '__main__':
-    unittest.main()
+# if __name__ == '__main__':
+#     unittest.main()
