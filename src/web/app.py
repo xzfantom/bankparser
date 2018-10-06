9e7d97de38f1919ab37b766f732105382a1c640a
Artsiom Martynau <xzfantom@gmail.com>
2018-10-06 21:26:42 +0300

    ответ

--- src/web/app.py
+++ src/web/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, json
 
 app = Flask(__name__)
 
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
-@app.route("/parse")
+@app.route("/parse", methods=['POST'])
 def parse():
-    return render_template('index.html')
+    return json.dumps({'html':'<span>All fields good !!</span>'})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
